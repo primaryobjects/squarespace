f595b8d2223a525bbc3b850075223f38574c4442
Kory Becker <kbecker@primaryobjects.com>
2021-06-27 16:48:28 -0400

    Update collections.js

--- collections.js
+++ collections.js
@@ -2,22 +2,74 @@
 const Author = props => {
   const author = props.data;
   return (
-    <div className="col sqs-col-6 span-6" id="yui_3_17_2_1_1624821117171_521"><div className="row sqs-row" id="yui_3_17_2_1_1624821117171_520"><div className="col sqs-col-5 span-5" id="yui_3_17_2_1_1624821117171_519"><div className="sqs-block image-block sqs-block-image sqs-col-3 span-3 float float-right sqs-text-ready" data-block-type={5} id="block-yui_3_17_2_1_1601414526373_54455"><div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_518">
-          <div className="image-block-outer-wrapper layout-caption-below design-layout-inline combination-animation-site-default individual-animation-site-default individual-text-animation-site-default sqs-narrow-width animation-loaded" data-test="image-block-inline-outer-wrapper" id="yui_3_17_2_1_1624821117171_517">
-            <figure className="
-        sqs-block-image-figure
-        intrinsic
-      " style={{maxWidth: '828px'}} id="yui_3_17_2_1_1624821117171_516">
-              <div style={{paddingBottom: '113.889%', overflow: 'hidden'}} className="
-          image-block-wrapper
-          has-aspect-ratio
-        " data-animation-role="image" id="yui_3_17_2_1_1624821117171_515">
-                <img className="thumb-image loaded" data-src={author.profileImage} data-image={author.profileImage} data-image-dimensions="828x943" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="5f73a64abdc51f78805cb242" data-type="image" style={{left: '0%', top: '-0.119642%', width: '100%', height: '100.239%', position: 'absolute'}} data-image-resolution="300w" src={author.profileImage} />
-              </div>
-            </figure>
+    <div className="col sqs-col-6 span-6" id="yui_3_17_2_1_1624821117171_521">
+       <div className="row sqs-row" id="yui_3_17_2_1_1624821117171_520">
+          <div className="col sqs-col-5 span-5" id="yui_3_17_2_1_1624821117171_519">
+             <div className="sqs-block image-block sqs-block-image sqs-col-3 span-3 float float-right sqs-text-ready" data-block-type={5} id="block-yui_3_17_2_1_1601414526373_54455">
+                <div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_518">
+                   <div className="image-block-outer-wrapper layout-caption-below design-layout-inline combination-animation-site-default individual-animation-site-default individual-text-animation-site-default sqs-narrow-width animation-loaded" data-test="image-block-inline-outer-wrapper" id="yui_3_17_2_1_1624821117171_517">
+                      <figure className="
+                      sqs-block-image-figure
+                      intrinsic
+                      " style={{maxWidth: '828px'}} id="yui_3_17_2_1_1624821117171_516">
+                      <div style={{paddingBottom: '113.889%', overflow: 'hidden'}} className="
+                      image-block-wrapper
+                      has-aspect-ratio
+                      " data-animation-role="image" id="yui_3_17_2_1_1624821117171_515">
+                      <img className="thumb-image loaded" data-src={author.profileImage} data-image={author.profileImage} data-image-dimensions="828x943" data-image-focal-point="0.5,0.5" data-load="false" data-image-id="5f73a64abdc51f78805cb242" data-type="image" style={{left: '0%', top: '-0.119642%', width: '100%', height: '100.239%', position: 'absolute'}} data-image-resolution="300w" src={author.profileImage} />
+                   </div>
+                   </figure>
+                </div>
+             </div>
           </div>
-        </div></div><div className="sqs-block html-block sqs-block-html" data-block-type={2} id="block-1e88d17bec83c8e26856"><div className="sqs-block-content"><h4 style={{whiteSpace: 'pre-wrap'}}>{author.name}</h4><p className style={{whiteSpace: 'pre-wrap'}}>{author.biography}</p><p className style={{whiteSpace: 'pre-wrap'}}><strong>Connect with Paul:</strong></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="http://www.paullisicky.net/" target="_blank">http://www.paullisicky.net</a></p><p className style={{whiteSpace: 'pre-wrap'}}><strong>Buy Paul’s Books:</strong></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781644450161" target="_blank">Later: My Life at the Edge of the World</a></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781555977283" target="_blank">The Narrow Door: A Memoir of Friendship</a></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781935536253" target="_blank">Unbuilt Projects</a></p><h4 style={{whiteSpace: 'pre-wrap'}}>The Collingswood Book Festival is proud to present two videos featuring Paul. The first features a Q&amp;A between Paul and journalist Tammy Paolino. In the second video, Paul reads from his new memoir, <em>Later: My Life at the Edge of the World</em>.</h4></div></div><div className="sqs-block video-block sqs-block-video" data-block-json="{&quot;blockAnimation&quot;:&quot;none&quot;,&quot;layout&quot;:&quot;caption-below&quot;,&quot;overlay&quot;:true,&quot;description&quot;:{&quot;html&quot;:&quot;<p class=\&quot;\&quot;></p>&quot;,&quot;raw&quot;:false},&quot;hSize&quot;:null,&quot;floatDir&quot;:null,&quot;customThumb&quot;:&quot;5f73a6f756b13e52a1d8f9e4&quot;,&quot;html&quot;:&quot;<iframe src=\&quot;//www.youtube.com/embed/y1upm3VHLeY?wmode=opaque&amp;enablejsapi=1\&quot; height=\&quot;480\&quot; width=\&quot;854\&quot; scrolling=\&quot;no\&quot; frameborder=\&quot;0\&quot; allowfullscreen=\&quot;\&quot;>\n</iframe>&quot;,&quot;url&quot;:&quot;https://youtu.be/y1upm3VHLeY&quot;,&quot;resolvedBy&quot;:&quot;youtube&quot;}" data-block-type={32} id="block-yui_3_17_2_1_1601413067615_25681"><div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_154"><div className="sqs-video-wrapper video-none" data-html="<iframe src=&quot;//www.youtube.com/embed/y1upm3VHLeY?wmode=opaque&enablejsapi=1&quot; height=&quot;480&quot; width=&quot;854&quot; scrolling=&quot;no&quot; frameborder=&quot;0&quot; allowfullscreen=&quot;&quot;><br/></iframe>" data-provider-name id="yui_3_17_2_1_1624821117171_161"><div className="intrinsic"><div className="intrinsic-inner" style={{paddingBottom: '56.2061%'}}><div className="sqs-video-overlay" style={{opacity: 1, overflow: 'hidden'}} id="yui_3_17_2_1_1624821117171_180"><img data-src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414903976-32ZUF3XSAGTPQOT21DYW/Paul_Tammy.jpg" data-load="false" data-image-focal-point="0.5,0.5" className="loaded" data-image-dimensions="100x56" data-parent-ratio="1.8" style={{fontSize: '0px', left: '-0.464286px', top: '0px', width: '433.929px', height: '243px', position: 'relative'}} data-image-resolution="750w" src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414903976-32ZUF3XSAGTPQOT21DYW/Paul_Tammy.jpg?format=750w" /><div className="sqs-video-opaque" /><div className="sqs-video-icon" tabIndex={0} role="button" aria-label="Play" /></div></div></div></div><div className="video-caption-wrapper"><div className="video-caption"><p className /></div></div></div></div><div className="sqs-block spacer-block sqs-block-spacer sized vsize-1" data-block-type={21} id="block-yui_3_17_2_1_1601413767315_26757"><div className="sqs-block-content">&nbsp;</div></div><div className="sqs-block video-block sqs-block-video" data-block-json="{&quot;blockAnimation&quot;:&quot;none&quot;,&quot;layout&quot;:&quot;caption-below&quot;,&quot;overlay&quot;:true,&quot;description&quot;:{&quot;html&quot;:&quot;<p class=\&quot;\&quot;></p>&quot;,&quot;raw&quot;:false},&quot;hSize&quot;:null,&quot;floatDir&quot;:null,&quot;customThumb&quot;:&quot;5f73a7327352d517aa67ae39&quot;,&quot;html&quot;:&quot;<iframe src=\&quot;//www.youtube.com/embed/3sL6BbRDYIg?wmode=opaque&amp;enablejsapi=1\&quot; height=\&quot;480\&quot; width=\&quot;854\&quot; scrolling=\&quot;no\&quot; frameborder=\&quot;0\&quot; allowfullscreen=\&quot;\&quot;>\n</iframe>&quot;,&quot;url&quot;:&quot;https://youtu.be/3sL6BbRDYIg&quot;,&quot;resolvedBy&quot;:&quot;youtube&quot;}" data-block-type={32} id="block-yui_3_17_2_1_1601413767315_27529"><div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_213"><div className="sqs-video-wrapper video-none" data-html="<iframe src=&quot;//www.youtube.com/embed/3sL6BbRDYIg?wmode=opaque&enablejsapi=1&quot; height=&quot;480&quot; width=&quot;854&quot; scrolling=&quot;no&quot; frameborder=&quot;0&quot; allowfullscreen=&quot;&quot;><br/></iframe>" data-provider-name id="yui_3_17_2_1_1624821117171_220"><div className="intrinsic"><div className="intrinsic-inner" style={{paddingBottom: '56.2061%'}}><div className="sqs-video-overlay" style={{opacity: 1, overflow: 'hidden'}} id="yui_3_17_2_1_1624821117171_239"><img data-src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414962997-UQ54SZWY0I3LJ422Y01I/paul.jpg" data-load="false" data-image-focal-point="0.5,0.5" className="loaded" data-image-dimensions="100x56" data-parent-ratio="1.8" style={{fontSize: '0px', left: '-0.464286px', top: '0px', width: '433.929px', height: '243px', position: 'relative'}} data-image-resolution="750w" src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414962997-UQ54SZWY0I3LJ422Y01I/paul.jpg?format=750w" /><div className="sqs-video-opaque" /><div className="sqs-video-icon" tabIndex={0} role="button" aria-label="Play" /></div></div></div></div><div className="video-caption-wrapper"><div className="video-caption"><p className /></div></div></div></div></div><div className="col sqs-col-1 span-1"><div className="sqs-block spacer-block sqs-block-spacer sized vsize-1" data-block-type={21} id="block-f2bc30f609edf7e6a224"><div className="sqs-block-content">&nbsp;</div></div></div></div></div>
-
+          <div className="sqs-block html-block sqs-block-html" data-block-type={2} id="block-1e88d17bec83c8e26856">
+             <div className="sqs-block-content"><h4 style={{whiteSpace: 'pre-wrap'}}>{author.name}</h4><p className style={{whiteSpace: 'pre-wrap'}}>{author.biography}</p><p className style={{whiteSpace: 'pre-wrap'}}><strong>Connect with Paul:</strong></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="http://www.paullisicky.net/" target="_blank">http://www.paullisicky.net</a></p><p className style={{whiteSpace: 'pre-wrap'}}><strong>Buy Paul’s Books:</strong></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781644450161" target="_blank">Later: My Life at the Edge of the World</a></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781555977283" target="_blank">The Narrow Door: A Memoir of Friendship</a></p><p className style={{whiteSpace: 'pre-wrap'}}><a href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781935536253" target="_blank">Unbuilt Projects</a></p><h4 style={{whiteSpace: 'pre-wrap'}}>The Collingswood Book Festival is proud to present two videos featuring Paul. The first features a Q&amp;A between Paul and journalist Tammy Paolino. In the second video, Paul reads from his new memoir, <em>Later: My Life at the Edge of the World</em>.</h4></div>
+          </div>
+          <div className="sqs-block video-block sqs-block-video" data-block-json="{&quot;blockAnimation&quot;:&quot;none&quot;,&quot;layout&quot;:&quot;caption-below&quot;,&quot;overlay&quot;:true,&quot;description&quot;:{&quot;html&quot;:&quot;<p class=\&quot;\&quot;></p>&quot;,&quot;raw&quot;:false},&quot;hSize&quot;:null,&quot;floatDir&quot;:null,&quot;customThumb&quot;:&quot;5f73a6f756b13e52a1d8f9e4&quot;,&quot;html&quot;:&quot;<iframe src=\&quot;//www.youtube.com/embed/y1upm3VHLeY?wmode=opaque&amp;enablejsapi=1\&quot; height=\&quot;480\&quot; width=\&quot;854\&quot; scrolling=\&quot;no\&quot; frameborder=\&quot;0\&quot; allowfullscreen=\&quot;\&quot;>\n</iframe>&quot;,&quot;url&quot;:&quot;https://youtu.be/y1upm3VHLeY&quot;,&quot;resolvedBy&quot;:&quot;youtube&quot;}" data-block-type={32} id="block-yui_3_17_2_1_1601413067615_25681">
+             <div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_154">
+                <div className="sqs-video-wrapper video-none" data-html="<iframe src=&quot;//www.youtube.com/embed/y1upm3VHLeY?wmode=opaque&enablejsapi=1&quot; height=&quot;480&quot; width=&quot;854&quot; scrolling=&quot;no&quot; frameborder=&quot;0&quot; allowfullscreen=&quot;&quot;><br/></iframe>" data-provider-name id="yui_3_17_2_1_1624821117171_161">
+                   <div className="intrinsic">
+                      <div className="intrinsic-inner" style={{paddingBottom: '56.2061%'}}><div className="sqs-video-overlay" style={{opacity: 1, overflow: 'hidden'}} id="yui_3_17_2_1_1624821117171_180"><img data-src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414903976-32ZUF3XSAGTPQOT21DYW/Paul_Tammy.jpg" data-load="false" data-image-focal-point="0.5,0.5" className="loaded" data-image-dimensions="100x56" data-parent-ratio="1.8" style={{fontSize: '0px', left: '-0.464286px', top: '0px', width: '433.929px', height: '243px', position: 'relative'}} data-image-resolution="750w" src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414903976-32ZUF3XSAGTPQOT21DYW/Paul_Tammy.jpg?format=750w" />
+                      <div className="sqs-video-opaque" />
+                         <div className="sqs-video-icon" tabIndex={0} role="button" aria-label="Play" /></div>
+                      </div>
+                   </div>
+                </div>
+                <div className="video-caption-wrapper">
+                   <div className="video-caption">
+                      <p className />
+                   </div>
+                </div>
+             </div>
+          </div>
+          <div className="sqs-block spacer-block sqs-block-spacer sized vsize-1" data-block-type={21} id="block-yui_3_17_2_1_1601413767315_26757">
+             <div className="sqs-block-content">&nbsp;</div>
+          </div>
+          <div className="sqs-block video-block sqs-block-video" data-block-json="{&quot;blockAnimation&quot;:&quot;none&quot;,&quot;layout&quot;:&quot;caption-below&quot;,&quot;overlay&quot;:true,&quot;description&quot;:{&quot;html&quot;:&quot;<p class=\&quot;\&quot;></p>&quot;,&quot;raw&quot;:false},&quot;hSize&quot;:null,&quot;floatDir&quot;:null,&quot;customThumb&quot;:&quot;5f73a7327352d517aa67ae39&quot;,&quot;html&quot;:&quot;<iframe src=\&quot;//www.youtube.com/embed/3sL6BbRDYIg?wmode=opaque&amp;enablejsapi=1\&quot; height=\&quot;480\&quot; width=\&quot;854\&quot; scrolling=\&quot;no\&quot; frameborder=\&quot;0\&quot; allowfullscreen=\&quot;\&quot;>\n</iframe>&quot;,&quot;url&quot;:&quot;https://youtu.be/3sL6BbRDYIg&quot;,&quot;resolvedBy&quot;:&quot;youtube&quot;}" data-block-type={32} id="block-yui_3_17_2_1_1601413767315_27529">
+             <div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_213">
+                <div className="sqs-video-wrapper video-none" data-html="<iframe src=&quot;//www.youtube.com/embed/3sL6BbRDYIg?wmode=opaque&enablejsapi=1&quot; height=&quot;480&quot; width=&quot;854&quot; scrolling=&quot;no&quot; frameborder=&quot;0&quot; allowfullscreen=&quot;&quot;><br/></iframe>" data-provider-name id="yui_3_17_2_1_1624821117171_220">
+                   <div className="intrinsic">
+                      <div className="intrinsic-inner" style={{paddingBottom: '56.2061%'}}><div className="sqs-video-overlay" style={{opacity: 1, overflow: 'hidden'}} id="yui_3_17_2_1_1624821117171_239"><img data-src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414962997-UQ54SZWY0I3LJ422Y01I/paul.jpg" data-load="false" data-image-focal-point="0.5,0.5" className="loaded" data-image-dimensions="100x56" data-parent-ratio="1.8" style={{fontSize: '0px', left: '-0.464286px', top: '0px', width: '433.929px', height: '243px', position: 'relative'}} data-image-resolution="750w" src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414962997-UQ54SZWY0I3LJ422Y01I/paul.jpg?format=750w" />
+                      <div className="sqs-video-opaque" />
+                         <div className="sqs-video-icon" tabIndex={0} role="button" aria-label="Play" /></div>
+                      </div>
+                   </div>
+                </div>
+                <div className="video-caption-wrapper">
+                   <div className="video-caption">
+                      <p className />
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <div className="col sqs-col-1 span-1">
+          <div className="sqs-block spacer-block sqs-block-spacer sized vsize-1" data-block-type={21} id="block-f2bc30f609edf7e6a224">
+             <div className="sqs-block-content">&nbsp;</div>
+          </div>
+       </div>
+      </div>
+    </div>
   );
 }
 
