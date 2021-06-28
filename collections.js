495d75ec289f99b4a2ae9ad746e1306f3dac0e61
Kory Becker <kbecker@primaryobjects.com>
2021-06-27 21:39:50 -0400

    Update collections.js

--- collections.js
+++ collections.js
@@ -1,4 +1,94 @@
 // noprotect
+const ProfileImage = props => {
+  return (
+    <div
+    className="image-block-outer-wrapper layout-caption-below design-layout-inline combination-animation-site-default individual-animation-site-default individual-text-animation-site-default sqs-narrow-width animation-loaded"
+    data-test="image-block-inline-outer-wrapper"
+    id="yui_3_17_2_1_1624821117171_517"
+  >
+    <figure
+      className="
+        sqs-block-image-figure
+        intrinsic
+        "
+      style={{ maxWidth: "828px" }}
+      id="yui_3_17_2_1_1624821117171_516"
+    >
+      <div
+        style={{ paddingBottom: "113.889%", overflow: "hidden" }}
+        className="
+        image-block-wrapper
+        has-aspect-ratio
+        "
+        data-animation-role="image"
+        id="yui_3_17_2_1_1624821117171_515"
+      >
+        <img
+          className="thumb-image loaded"
+          data-src={props.url}
+          data-image={props.url}
+          data-image-dimensions="828x943"
+          data-image-focal-point="0.5,0.5"
+          data-load="false"
+          data-image-id="5f73a64abdc51f78805cb242"
+          data-type="image"
+          style={{
+            left: "0%",
+            top: "-0.119642%",
+            width: "100%",
+            height: "100.239%",
+            position: "absolute",
+          }}
+          data-image-resolution="300w"
+          src={props.url}
+        />
+      </div>
+    </figure>
+  </div>
+  );
+};
+
+const Links = props => {
+  const { website, facebook, instagram, twitter } = props.data;
+  
+  return (
+    <div className="links">
+      <p className style={{ whiteSpace: "pre-wrap" }}>
+        <strong>Connect with the Author:</strong>
+      </p>
+      <p className style={{ whiteSpace: "pre-wrap" }}>
+        <i className="fas fa-link" style={{ marginRight: "5px" }}></i>
+        <a href={website} target="_blank">
+          {website}
+        </a>
+      </p>
+      <p className style={{ whiteSpace: "pre-wrap" }}>
+        <i className="fab fa-facebook-f" style={{ marginRight: "5px" }}></i>
+        <a href={facebook} target="_blank">
+          {facebook}
+        </a>
+      </p>
+      <p className style={{ whiteSpace: "pre-wrap" }}>
+        <i className="fab fa-instagram" style={{ marginRight: "5px" }}></i>
+        <a href={instagram} target="_blank">
+          {instagram}
+        </a>
+      </p>
+      <p className style={{ whiteSpace: "pre-wrap" }}>
+        <i className="fab fa-twitter" style={{ marginRight: "5px" }}></i>
+        <a
+          href={
+            twitter.startsWith("http") ? twitter : `http://twitter.com/${twitter}`
+          }
+          target="_blank"
+        >
+          {twitter}
+        </a>
+      </p>
+    </div>
+  );
+};
+
 const Author = props => {
   const author = props.data;
   return (
@@ -11,50 +101,7 @@ const Author = props => {
             id="block-yui_3_17_2_1_1601414526373_54455"
           >
             <div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_518">
-              <div
-                className="image-block-outer-wrapper layout-caption-below design-layout-inline combination-animation-site-default individual-animation-site-default individual-text-animation-site-default sqs-narrow-width animation-loaded"
-                data-test="image-block-inline-outer-wrapper"
-                id="yui_3_17_2_1_1624821117171_517"
-              >
-                <figure
-                  className="
-                          sqs-block-image-figure
-                          intrinsic
-                          "
-                  style={{ maxWidth: "828px" }}
-                  id="yui_3_17_2_1_1624821117171_516"
-                >
-                  <div
-                    style={{ paddingBottom: "113.889%", overflow: "hidden" }}
-                    className="
-                          image-block-wrapper
-                          has-aspect-ratio
-                          "
-                    data-animation-role="image"
-                    id="yui_3_17_2_1_1624821117171_515"
-                  >
-                    <img
-                      className="thumb-image loaded"
-                      data-src={author.profileImage}
-                      data-image={author.profileImage}
-                      data-image-dimensions="828x943"
-                      data-image-focal-point="0.5,0.5"
-                      data-load="false"
-                      data-image-id="5f73a64abdc51f78805cb242"
-                      data-type="image"
-                      style={{
-                        left: "0%",
-                        top: "-0.119642%",
-                        width: "100%",
-                        height: "100.239%",
-                        position: "absolute",
-                      }}
-                      data-image-resolution="300w"
-                      src={author.profileImage}
-                    />
-                  </div>
-                </figure>
-              </div>
+              <ProfileImage url={author.profileImage}/>
             </div>
           </div>
           <div
@@ -67,184 +114,15 @@ const Author = props => {
               <p className style={{ whiteSpace: "pre-wrap" }}>
                 {author.biography}
               </p>
+              <Links data={author} />
               <p className style={{ whiteSpace: "pre-wrap" }}>
-                <strong>Connect with the Author:</strong>
+                <strong>Buy the Book:</strong>
               </p>
               <p className style={{ whiteSpace: "pre-wrap" }}>
-                <a href={author.website} target="_blank">
-                  {author.website}
-                </a>
+                {author.bookInformation}
               </p>
-              <p className style={{ whiteSpace: "pre-wrap" }}>
-                <a href={author.facebook} target="_blank">
-                  {author.facebook}
-                </a>
-              </p>
-              <p className style={{ whiteSpace: "pre-wrap" }}>
-                <a href={author.instagram} target="_blank">
-                  {author.instagram}
-                </a>
-              </p>
-              <p className style={{ whiteSpace: "pre-wrap" }}>
-                <a href={author.twitter.startsWith('http') ? author.twitter : `http://twitter.com/${author.twitter}`} target='_blank'>{author.twitter}</a>
-              </p>
-              <p className style={{ whiteSpace: "pre-wrap" }}>
-                <strong>Buy the Books:</strong>
-              </p>
-              <p className style={{ whiteSpace: "pre-wrap" }}>
-                <a
-                  href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781644450161"
-                  target="_blank"
-                >
-                  Later: My Life at the Edge of the World
-                </a>
-              </p>
-              <p className style={{ whiteSpace: "pre-wrap" }}>
-                <a
-                  href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781555977283"
-                  target="_blank"
-                >
-                  The Narrow Door: A Memoir of Friendship
-                </a>
-              </p>
-              <p className style={{ whiteSpace: "pre-wrap" }}>
-                <a
-                  href="https://inkwoodnj.handseller.com/home/bookdetailsin/9781935536253"
-                  target="_blank"
-                >
-                  Unbuilt Projects
-                </a>
-              </p>
-              <h4 style={{ whiteSpace: "pre-wrap" }}>
-                The Collingswood Book Festival is proud to present two videos
-                featuring Paul. The first features a Q&amp;A between Paul and
-                journalist Tammy Paolino. In the second video, Paul reads from his
-                new memoir, <em>Later: My Life at the Edge of the World</em>.
-              </h4>
-            </div>
-          </div>
-          <div
-            className="sqs-block video-block sqs-block-video"
-            data-block-json='{"blockAnimation":"none","layout":"caption-below","overlay":true,"description":{"html":"<p class=\"\"></p>","raw":false},"hSize":null,"floatDir":null,"customThumb":"5f73a6f756b13e52a1d8f9e4","html":"<iframe src=\"//www.youtube.com/embed/y1upm3VHLeY?wmode=opaque&amp;enablejsapi=1\" height=\"480\" width=\"854\" scrolling=\"no\" frameborder=\"0\" allowfullscreen=\"\">\n</iframe>","url":"https://youtu.be/y1upm3VHLeY","resolvedBy":"youtube"}'
-            data-block-type={32}
-            id="block-yui_3_17_2_1_1601413067615_25681"
-          >
-            <div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_154">
-              <div
-                className="sqs-video-wrapper video-none"
-                data-html='<iframe src="//www.youtube.com/embed/y1upm3VHLeY?wmode=opaque&enablejsapi=1" height="480" width="854" scrolling="no" frameborder="0" allowfullscreen=""><br/></iframe>'
-                data-provider-name
-                id="yui_3_17_2_1_1624821117171_161"
-              >
-                <div className="intrinsic">
-                  <div
-                    className="intrinsic-inner"
-                    style={{ paddingBottom: "56.2061%" }}
-                  >
-                    <div
-                      className="sqs-video-overlay"
-                      style={{ opacity: 1, overflow: "hidden" }}
-                      id="yui_3_17_2_1_1624821117171_180"
-                    >
-                      <img
-                        data-src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414903976-32ZUF3XSAGTPQOT21DYW/Paul_Tammy.jpg"
-                        data-load="false"
-                        data-image-focal-point="0.5,0.5"
-                        className="loaded"
-                        data-image-dimensions="100x56"
-                        data-parent-ratio="1.8"
-                        style={{
-                          fontSize: "0px",
-                          left: "-0.464286px",
-                          top: "0px",
-                          width: "433.929px",
-                          height: "243px",
-                          position: "relative",
-                        }}
-                        data-image-resolution="750w"
-                        src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414903976-32ZUF3XSAGTPQOT21DYW/Paul_Tammy.jpg?format=750w"
-                      />
-                      <div className="sqs-video-opaque" />
-                      <div
-                        className="sqs-video-icon"
-                        tabIndex={0}
-                        role="button"
-                        aria-label="Play"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="video-caption-wrapper">
-                <div className="video-caption">
-                  <p className />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className="sqs-block spacer-block sqs-block-spacer sized vsize-1"
-            data-block-type={21}
-            id="block-yui_3_17_2_1_1601413767315_26757"
-          >
-            <div className="sqs-block-content">&nbsp;</div>
-          </div>
-          <div
-            className="sqs-block video-block sqs-block-video"
-            data-block-json='{"blockAnimation":"none","layout":"caption-below","overlay":true,"description":{"html":"<p class=\"\"></p>","raw":false},"hSize":null,"floatDir":null,"customThumb":"5f73a7327352d517aa67ae39","html":"<iframe src=\"//www.youtube.com/embed/3sL6BbRDYIg?wmode=opaque&amp;enablejsapi=1\" height=\"480\" width=\"854\" scrolling=\"no\" frameborder=\"0\" allowfullscreen=\"\">\n</iframe>","url":"https://youtu.be/3sL6BbRDYIg","resolvedBy":"youtube"}'
-            data-block-type={32}
-            id="block-yui_3_17_2_1_1601413767315_27529"
-          >
-            <div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_213">
-              <div
-                className="sqs-video-wrapper video-none"
-                data-html='<iframe src="//www.youtube.com/embed/3sL6BbRDYIg?wmode=opaque&enablejsapi=1" height="480" width="854" scrolling="no" frameborder="0" allowfullscreen=""><br/></iframe>'
-                data-provider-name
-                id="yui_3_17_2_1_1624821117171_220"
-              >
-                <div className="intrinsic">
-                  <div
-                    className="intrinsic-inner"
-                    style={{ paddingBottom: "56.2061%" }}
-                  >
-                    <div
-                      className="sqs-video-overlay"
-                      style={{ opacity: 1, overflow: "hidden" }}
-                      id="yui_3_17_2_1_1624821117171_239"
-                    >
-                      <img
-                        data-src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414962997-UQ54SZWY0I3LJ422Y01I/paul.jpg"
-                        data-load="false"
-                        data-image-focal-point="0.5,0.5"
-                        className="loaded"
-                        data-image-dimensions="100x56"
-                        data-parent-ratio="1.8"
-                        style={{
-                          fontSize: "0px",
-                          left: "-0.464286px",
-                          top: "0px",
-                          width: "433.929px",
-                          height: "243px",
-                          position: "relative",
-                        }}
-                        data-image-resolution="750w"
-                        src="https://images.squarespace-cdn.com/content/v1/5f1cb0d99322d817a05021ad/1601414962997-UQ54SZWY0I3LJ422Y01I/paul.jpg?format=750w"
-                      />
-                      <div className="sqs-video-opaque" />
-                      <div
-                        className="sqs-video-icon"
-                        tabIndex={0}
-                        role="button"
-                        aria-label="Play"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="video-caption-wrapper">
-                <div className="video-caption">
-                  <p className />
-                </div>
+              <div className="sqs-block-content" id="yui_3_17_2_1_1624821117171_518">
+                <img src={author.bookImage} style={{ maxWidth: '200px' }}/>
               </div>
             </div>
           </div>
@@ -338,6 +216,7 @@ const adultAuthorManager = {
     return {
       name: row['gsx$author']['$t'],
       biography: row['gsx$biography']['$t'],
+      bookInformation: row['gsx$bookinformation']['$t'],
       website: row['gsx$website']['$t'],
       facebook: row['gsx$facebook']['$t'],
       instagram: row['gsx$instagram']['$t'],
